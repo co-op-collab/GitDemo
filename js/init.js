d746e49119271192a20caa1eaf00d027b0e6c7fb
yorba <liamxlynch@gmail.com>
2017-01-17 10:10:20 +0800

    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+
+    $(".button-collapse").sideNav();//mobile screen menu init
+
+    $('.carousel').carousel(); //carousel init
+
+    $('.carousel.carousel-slider').carousel({full_width: true});
+});
+             
+    
+
+        
